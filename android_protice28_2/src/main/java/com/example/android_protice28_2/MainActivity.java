f5659e1147a492642ba7fdeeeeed9030952c2040
Your Name <email@example.com>
2016-09-25 19:16:00 +0800

    Build protice 19to21. changed protice19.1

--- android_protice28_2/src/main/java/com/example/android_protice28_2/MainActivity.java
+++ android_protice28_2/src/main/java/com/example/android_protice28_2/MainActivity.java
@@ -8,13 +8,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
-import java.io.IOException;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "hylTag";
     private Button btnStart;
     private Button btnStop;
+    private Button btnPause;
     private MediaPlayer mediaPlayer;
 
 
@@ -25,9 +25,11 @@ public class MainActivity extends AppCompatActivity {
 
         btnStart = (Button) findViewById(R.id.btnStart);
         btnStop = (Button) findViewById(R.id.btnStop);
+        btnPause = (Button) findViewById(R.id.btnPause);
 
         mediaPlayer = new MediaPlayer();
         btnStop.setEnabled(false);
+        btnPause.setEnabled(false);
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,21 +39,42 @@ public class MainActivity extends AppCompatActivity {
                     mediaPlayer.reset();
 
                     AssetManager assetManager = getAssets();
-                    AssetFileDescriptor assetFileDescriptor = null;
-                    try {
-                        assetFileDescriptor = assetManager.openFd("stronger.mp3");
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    mediaPlayer.setDataSource(assetFileDescriptor.getFileDescriptor());
+
+                    AssetFileDescriptor assetFileDescriptor = assetManager.openFd("stronger.mp3");
+                    mediaPlayer.setDataSource(assetFileDescriptor.getFileDescriptor(), assetFileDescriptor.getStartOffset(), assetFileDescriptor.getLength());
+                    Toast.makeText(MainActivity.this, "开始", Toast.LENGTH_LONG).show();
                     mediaPlayer.prepare();
                     mediaPlayer.start();
 
                     btnStop.setEnabled(true);
+                    btnPause.setEnabled(true);
 
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
+            }
+        });
+        //暂停按钮
+        btnPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer.isPlaying()) {
+                    btnPause.setText("继续");
+                    mediaPlayer.pause();
+                } else {
+                    btnPause.setText("暂停");
+                    mediaPlayer.start();
+                }
+            }
+        });
+        //停止按钮
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.stop();
+                btnPause.setText("暂停");
+                btnPause.setEnabled(false);
+                btnStop.setEnabled(false);
             }
         });
      /*   btnStart.setOnClickListener(new View.OnClickListener() {
