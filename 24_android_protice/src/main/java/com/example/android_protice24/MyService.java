58036365e19ee04ceeb8b74725157086f5994556
Your Name <email@example.com>
2016-10-16 18:47:25 +0800

    修正几个练习，把之前没上传的上传

---
+++ 24_android_protice/src/main/java/com/example/android_protice24/MyService.java
@@ -0,0 +1,48 @@
+package com.example.android_protice24;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+/**
+ * Created by Administrator on 2016/10/15.
+ */
+public class MyService extends Service {
+    private static final String TAG = "myServicetag";
+
+    public MyService() {
+    }
+
+    @Override
+    public void onCreate() {
+        Log.v(TAG, "onCreate()");
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.v(TAG, "onStartCommand()");
+        int n = intent.getIntExtra("num", 0);
+        Log.v(TAG, "int is:" + n);
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.v(TAG, "onDestroy()");
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.v(TAG, "onBind()");
+        return null;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.v(TAG, "onUnbind()");
+        return super.onUnbind(intent);
+    }
+}
